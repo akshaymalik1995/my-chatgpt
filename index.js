95e2ca50dba0b6836d92e48829aa9be1cb2c4dc0
akshaymalik1995 <akshaymalik191@gmail.com>
2023-11-12 09:16:55 +0530

    Separation of tabs

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const promptForm = document.querySelector("#prompt-form")
 const keyForm = document.querySelector("#key-form")
 
-function handleKeyForm(e){
+
+
+
+
+function handleKeyForm(e) {
     e.preventDefault()
     form = e.target
     const key = form['key'].value
     if (!key) {
-         return
+        return
     }
     localStorage.setItem('key', key)
     addInfo("Key is added")
@@ -14,7 +18,7 @@ function handleKeyForm(e){
 }
 
 
-function addInfo(info){
+function addInfo(info) {
     const target = document.querySelector("#info")
     target.textContent = info
     target.style.display = "block"
@@ -22,24 +26,24 @@ function addInfo(info){
         target.textContent = ""
         target.style.display = "none"
     }, 2000)
-    
+
 }
 
 keyForm.addEventListener("submit", handleKeyForm)
 
-function disableForm(form, disabled){
+function disableForm(form, disabled) {
     const elements = form.elements
-    for (let i = 0; i < elements.length ; i++){
+    for (let i = 0; i < elements.length; i++) {
         elements[i].disabled = disabled
     }
 }
 
-function injectResponse(content){
+function injectResponse(content) {
     const target = document.querySelector("#answer")
     target.innerText = content
 }
 
-async function handlePromptForm(e){
+async function handlePromptForm(e) {
     e.preventDefault()
     const form = e.target
     let userPrompt = form['user-prompt'].value
@@ -52,8 +56,8 @@ async function handlePromptForm(e){
         addInfo("Key is missing")
         return
     }
-    
-    
+
+
     let selectedModel = form['selected-model'].value
     try {
         console.log("Loading...")
@@ -65,52 +69,52 @@ async function handlePromptForm(e){
             openai_key = key,
             model = selectedModel
         )
-        
+
         const content = data.choices[0].message.content
         injectResponse(content)
         console.log(content)
-        
-    } catch(err) {
+
+    } catch (err) {
         console.log(err)
         injectResponse("Something went wrong! Try again.")
     } finally {
-        disableForm(form, disabled=false)
+        disableForm(form, disabled = false)
     }
 }
 
 promptForm.addEventListener("submit", handlePromptForm)
 
-async function talkToGPT(systemPrompt, userPrompt, openai_key, model = "gpt-3.5-turbo", maxTokens = 1024){
-    if (!systemPrompt || !userPrompt || !openai_key){
+async function talkToGPT(systemPrompt, userPrompt, openai_key, model = "gpt-3.5-turbo", maxTokens = 1024) {
+    if (!systemPrompt || !userPrompt || !openai_key) {
         throw new Error("System prompt or user prompt or openai key is missing.")
     }
-    
+
     const url = "https://api.openai.com/v1/chat/completions"
     const headers = {
-        "Content-Type" : "application/json",
-        "Authorization" : `Bearer ${openai_key}`
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${openai_key}`
     }
 
     const body = {
-        model : model,
-        max_tokens : maxTokens,
-        messages : [
+        model: model,
+        max_tokens: maxTokens,
+        messages: [
             {
-                "role" : "system",
-                "content" : systemPrompt,
+                "role": "system",
+                "content": systemPrompt,
             },
             {
-                "role" : "user",
-                "content" : userPrompt,
+                "role": "user",
+                "content": userPrompt,
             },
         ]
     }
 
     try {
         const response = await fetch(url, {
-            method : "POST",
-            headers : headers,
-            body : JSON.stringify(body) 
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(body)
         })
         let data = await response.json()
         if (!response.ok) {
@@ -119,8 +123,35 @@ async function talkToGPT(systemPrompt, userPrompt, openai_key, model = "gpt-3.5-
         }
         return data
 
-    } catch(err) {
+    } catch (err) {
         console.error(err)
     }
 }
 
+const navigation = {
+    active: "home",
+    setCurrentTab() {
+        document.querySelector(`#${this.active}`).style.display = 'block'
+    },
+    hideCurrentTab() {
+        document.querySelector(`#${this.active}`).style.display = 'none'
+    },
+    changeTab(tab) {
+        this.hideCurrentTab()
+        this.active = tab
+        this.setCurrentTab()
+    },
+    initiate(){
+        const tabs = document.querySelectorAll("nav > div")
+        tabs.forEach(tab => {
+            const tabId = tab.dataset.target
+            tab.addEventListener("click", e => {
+                console.log(e)
+                navigation.changeTab(tabId)
+            })
+        })
+        this.setCurrentTab()
+    }
+}
+
+navigation.initiate()
